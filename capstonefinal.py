f47e255ad35c0ccd7696618b718861c0b21ca901
faw3 <100103873+faw3@users.noreply.github.com>
2022-08-31 15:02:49 +0300

    Update capstonefinal.py

--- capstonefinal.py
+++ capstonefinal.py
@@ -41,14 +41,15 @@ st.set_page_config(layout="wide",page_title=None)
     
 #dff=pd.read_csv("CosmalineTransactions.csv")
 
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-   dff = pd.read_csv(uploaded_file)
-if uploaded_file is None:
-   # To read file as bytes:
-   url = "https://drive.google.com/file/d/1vCbOYxh8aE9ZdAJWmeItOww4vQflIeau/view?usp=sharing"
-   path = 'https://drive.google.com/uc?export=download&id='+url.split('/')[-2]
-   dff = pd.read_csv(path)
+with st.sidebar:
+   uploaded_file = st.file_uploader("Choose a file")
+   if uploaded_file is not None:
+      dff = pd.read_csv(uploaded_file)
+   if uploaded_file is None:
+      # To read file as bytes:
+      url = "https://drive.google.com/file/d/1vCbOYxh8aE9ZdAJWmeItOww4vQflIeau/view?usp=sharing"
+      path = 'https://drive.google.com/uc?export=download&id='+url.split('/')[-2]
+      dff = pd.read_csv(path)
       
      
 
