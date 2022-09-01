c8671654db52a7049ae6a85c090ef307d08dd92b
faw3 <100103873+faw3@users.noreply.github.com>
2022-09-01 12:01:02 +0300

    Update capstonefinal.py

--- capstonefinal.py
+++ capstonefinal.py
@@ -1317,7 +1317,7 @@ if menu_id == "RFM Analysis":
         st.subheader(rfm_score)
     with col7:
         st.markdown("Area")
-        st.subheader(df2.iloc[0,61])
+        st.subheader(df2.iloc[0,35])
         
         
     with col8:
